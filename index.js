79d8b6f2e035712b784bfa4db02391c49278cc28
Emilio P <ejpascualj@gmail.com>
2021-05-30 16:39:00 -0500

    added create functionality for employee, role, and dept

--- index.js
+++ index.js
@@ -13,19 +13,23 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
-    runSearch();
+    initialize();
 });
 
-const runSearch = () => {
+const initialize = () => {
     inquirer.prompt({
         name: 'action',
         type: 'list',
         message: 'What would you like to do',
         choices:[
             'View all Employees',
+            'View all Roles',
+            'View all Departments',
             'View All Employees by Department',
             'View All Employees by Manager',
             'Add Employee',
+            'Add Department',
+            'Add Role',
             'Remove Employee',
             'Update Employee Role',
             'Update Employee Manager',
@@ -37,6 +41,12 @@ const runSearch = () => {
             case 'View all Employees':
                 ViewAllEmployees();
                 break;
+            case 'View all Roles':
+                ViewAllRoles();
+                break;
+            case 'View all Departments':
+                ViewAllDepartments();
+                break;
             case 'View All Employees by Department':
                 ViewEmpByDept();
                 break;
@@ -45,6 +55,12 @@ const runSearch = () => {
                 break;
             case 'Add Employee':
                 AddEmployee();
+                break;
+            case 'Add Department':
+                AddDepartment();
+                break;
+            case 'Add Role':
+                AddRole();
                 break;
             case 'Remove Employee':
                 RemoveEmployee()
@@ -66,10 +82,125 @@ const runSearch = () => {
     })
 };
 
+const GoBack = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'Would you like to Go back to Main Menu or Exit the app?',
+        choices:[
+            'Go Back',
+            'EXIT'
+        ]
+    })
+    .then((answer) => {
+        switch(answer.action) {
+            case 'Go Back':
+                initialize();
+                break;
+            case 'EXIT':
+                console.log('Good Bye!');
+                connection.end();
+                break;
+            default:
+                console.log(`Invalid action: ${answer.action}`);
+                break;                         
+        }
+    })
+};
+
 const ViewAllEmployees = () => {
     connection.query(queries.viewAllEmployees, (err, res) => {
         if (err) throw err;
         console.table(res);
-        connection.end();
+        GoBack();
     })
-}
+};
+
+const ViewAllRoles = () => {
+    connection.query(queries.viewAllEmployees, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        GoBack();
+    })
+};
+
+const ViewAllDepartments = () => {
+    connection.query(queries.viewAllEmployees, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        GoBack();
+    })
+};
+
+const AddEmployee = () => {
+    inquirer.prompt([
+        {
+            name: 'first_name',
+            type: 'input',
+            message: 'Add new employees first name',
+        },
+        {
+            name: 'last_name',
+            type: 'input',
+            message: 'Add new employees last name',
+        },
+        {
+            name: 'role_id',
+            type: 'input',
+            message: 'Add new employees role id',
+        },
+    ])
+    .then((answer) => {
+        connection.query(queries.createEmployee, [answer.first_name, answer.last_name, answer.role_id], (err, res) =>{
+            if (err) throw err;
+            console.log('Employee Succesfully Added!');
+            ViewAllEmployees();
+        })
+    })
+};
+
+const AddDepartment = () => {
+    inquirer.prompt({
+            name: 'department_name',
+            type: 'input',
+            message: 'Add new department name',
+    })
+    .then((answer) => {
+        connection.query(queries.createDepartment, answer.department_name, (err, res) =>{
+            if (err) throw err;
+            console.log('New Department Succesfully Added!');
+            ViewAllDepartments();
+            GoBack();
+        })
+    })
+};
+
+const AddRole = () => {
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'Add role title'
+        },
+        {
+            name: 'salary',
+            type: 'number',
+            message: 'Add role salary'
+        },
+        {
+            name: 'department_id',
+            type: 'number',
+            message: 'Add new department id'
+        }
+    ])
+    .then((answer) => {
+        connection.query(queries.createRole, [answer.title, answer.salary, answer.department_id], (err, res) =>{
+            if (err) throw err;
+            console.log('Role Succesfully Added!');
+            ViewAllRoles();
+            GoBack();
+        })
+    })
+};
+
+
--- queries.js
+++ queries.js
@@ -1,5 +1,9 @@
 const viewAllEmployees = 'SELECT employee.employee_id, employee.first_name, employee.last_name, role.title, department.name, role.salary, IFNULL(CONCAT(m.first_name, " ", m.last_name), NULL) AS Manager FROM employee LEFT JOIN employee m ON employee.manager_id = m.employee_id LEFT JOIN role ON employee.role_id = role.role_id LEFT JOIN department    ON role.department_id = department.department_id;';
 
+const viewDepartment = 'SELECT * FROM department'
+
+const viewRole = 'SELECT * FROM role'
+
 const createDepartment = 'INSERT INTO department (name) VALUES (?);';
 
 const createRole = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?);';
@@ -9,6 +13,8 @@ const createEmployee = 'INSERT INTO employee (first_name, last_name, role_id) VA
 const updateEmployeeRole = 'UPDATE employee SET role_id = ? WHERE employee_id = ?;';
 
 exports.viewAllEmployees = viewAllEmployees;
+exports.viewDepartment = viewDepartment;
+exports.viewRole = viewRole;
 exports.createDepartment = createDepartment;
 exports.createRole = createRole;
 exports.createEmployee = createEmployee;
