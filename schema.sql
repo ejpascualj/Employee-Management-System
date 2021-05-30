91a50e6dfb238cf2797468f72d9d04b02859e340
Emilio P <ejpascualj@gmail.com>
2021-05-30 13:24:41 -0500

    fixed issues with seed

--- schema.sql
+++ schema.sql
@@ -20,10 +20,10 @@ CREATE TABLE role (
 CREATE TABLE employee (
 	employee_id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30),
-     last_name VARCHAR(30),
-     role_id INT,
-     manager_id INT,
-     PRIMARY KEY (employee_id),
-     FOREIGN KEY (role_id) REFERENCES role(role_id),
-     FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,8 +1,10 @@
 USE EMS_db;
 
+-- CREATE DEPARTMENTS 
 INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
+-- CREATE ROLES
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1), 
 	("Salesperson", 80000, 1), 
@@ -12,12 +14,24 @@ VALUES ("Sales Lead", 100000, 1),
     ("Legal Team Lead", 250000, 4), 
     ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, 3),
-	("Mike", "Chan", 2, 1),
-    ("Ashley", "Rodriguez", 3, NULL),
-    ("Kevin", "Tupik", 4, 3),
-    ("Malia", "Brown", 5, NULL),
-    ("Sarah", "Lourd", 6, NULL),
-    ("Tom", "Allen", 7, 6),
-    ("Christian", "Eckenrode", 3, 2);
+-- CREATE EMPLOYEES 
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+	("Mike", "Chan", 2),
+    ("Ashley", "Rodriguez", 3),
+    ("Kevin", "Tupik", 4),
+    ("Malia", "Brown", 5),
+    ("Sarah", "Lourd", 6),
+    ("Tom", "Allen", 7),
+    ("Christian", "Eckenrode", 3);
+
+-- ADD MANAGERS TO EXISTING EMPLOYEES
+UPDATE employee SET manager_id = 3 WHERE  employee_id = 1;
+UPDATE employee SET manager_id = 1 WHERE  employee_id = 2;
+UPDATE employee SET manager_id = 3 WHERE  employee_id = 4;
+UPDATE employee SET manager_id = 6 WHERE  employee_id = 7;
+UPDATE employee SET manager_id = 2 WHERE  employee_id = 8;
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
